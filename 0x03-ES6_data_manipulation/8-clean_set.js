711a20198d1415df3ef9eda90fe8bef22a88d604
sosthene <sosthenetimi94@gmail.com>
2023-12-04 21:11:43 +0300

    Slicing strings and concatenating them

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) {
+  if (startString.length === 0 || startString === undefined) {
     return '';
-  } else {
-    let result = '';
-    set.forEach((value) => {
-      if (value.startsWith(startString)) {
-        result += `${value.slice(startString.length)}-`;
-      }
-    });
-    return result.slice(0, -1);
   }
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
+    .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
+    .join('-');
 }
